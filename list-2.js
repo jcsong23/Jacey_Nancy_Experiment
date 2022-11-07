f4e723e073876026e43ff89af0c69681d083c0c7
Nancy Xing <nxing@bowdoin.edu>
2022-11-07 11:44:46 -0500

    Added file path support for experimental images

--- list-2.js
+++ list-2.js
@@ -1,218 +1,266 @@
 var list2 = [
-    {
-      "object_number": 1,
-      "object": "watermelon",
-      "condition": "neutral-slice",
-      "sentence_type": "neutral",
-      "image_type": "slice",
-      "picture": "watermelon_slice.jpeg",
-      "sentence": "Someone places a watermelon in front of you."
-    },
-    {
-      "object_number": 2,
-      "object": "apple",
-      "condition": "neutral-slice",
-      "sentence_type": "neutral",
-      "image_type": "slice",
-      "picture": "apple_slice.jpeg",
-      "sentence": "Someone places an apple in front of you."
-    },
-    {
-      "object_number": 3,
-      "object": "cake",
-      "condition": "neutral-slice",
-      "sentence_type": "neutral",
-      "image_type": "slice",
-      "picture": "cake_slice.jpeg",
-      "sentence": "Someone places a cake in front of you."
-    },
-    {
-      "object_number": 4,
-      "object": "pie",
-      "condition": "neutral-slice",
-      "sentence_type": "neutral",
-      "image_type": "slice",
-      "picture": "pie _slice.jpeg",
-      "sentence": "Someone places a pie in front of you."
-    },
-    {
-      "object_number": 5,
-      "object": "bread",
-      "condition": "neutral-slice",
-      "sentence_type": "neutral",
-      "image_type": "slice",
-      "picture": "bread_slice.jpeg",
-      "sentence": "Someone places bread in front of you."
-    },
-    {
-      "object_number": 6,
-      "object": "pineapple",
-      "condition": "neutral-slice",
-      "sentence_type": "neutral",
-      "image_type": "slice",
-      "picture": "pineapple_slice.jpeg",
-      "sentence": "Someone places a pineapple in front of you."
-    },
-    {
-      "object_number": 7,
-      "object": "peach",
-      "condition": "neutral-whole",
-      "sentence_type": "neutral",
-      "image_type": "whole",
-      "picture": "peach_whole.jpeg",
-      "sentence": "Someone places a peach in front of you."
-    },
-    {
-      "object_number": 8,
-      "object": "steak",
-      "condition": "neutral-whole",
-      "sentence_type": "neutral",
-      "image_type": "whole",
-      "picture": "steak_whole.jpeg",
-      "sentence": "Someone places a steak in front of you."
-    },
-    {
-      "object_number": 9,
-      "object": "cheesecake",
-      "condition": "neutral-whole",
-      "sentence_type": "neutral",
-      "image_type": "whole",
-      "picture": "cheesecake_whole.jpeg",
-      "sentence": "Someone places a cheesecake in front of you."
-    },
-    {
-      "object_number": 10,
-      "object": "lemon",
-      "condition": "neutral-whole",
-      "sentence_type": "neutral",
-      "image_type": "whole",
-      "picture": "lemon_whole.jpeg",
-      "sentence": "Someone places a lemon in front of you."
-    },
-    {
-      "object_number": 11,
-      "object": "tomato",
-      "condition": "neutral-whole",
-      "sentence_type": "neutral",
-      "image_type": "whole",
-      "picture": "tomato_whole.jpeg",
-      "sentence": "Someone places a tomato in front of you."
-    },
-    {
-      "object_number": 12,
-      "object": "orange",
-      "condition": "neutral-whole",
-      "sentence_type": "neutral",
-      "image_type": "whole",
-      "picture": "orange_whole.jpeg",
-      "sentence": "Someone places an orange in front of you."
-    },
-    {
-      "object_number": 13,
-      "object": "pizza",
-      "condition": "modified-slice",
-      "sentence_type": "modified",
-      "image_type": "slice",
-      "picture": "pizza_slice.jpeg",
-      "sentence": "Someone slices a pizza in front of you."
-    },
-    {
-      "object_number": 14,
-      "object": "melon",
-      "condition": "modified-slice",
-      "sentence_type": "modified",
-      "image_type": "slice",
-      "picture": "melon_slice.jpeg",
-      "sentence": "Someone slices a melon in front of you."
-    },
-    {
-      "object_number": 15,
-      "object": "cucumber",
-      "condition": "modified-slice",
-      "sentence_type": "modified",
-      "image_type": "slice",
-      "picture": "cucumber_slice.jpeg",
-      "sentence": "Someone slices a cucumber in front of you."
-    },
-    {
-      "object_number": 16,
-      "object": "carrot",
-      "condition": "modified-slice",
-      "sentence_type": "modified",
-      "image_type": "slice",
-      "picture": "carrot_slice.jpeg",
-      "sentence": "Someone slices a carrot in front of you."
-    },
-    {
-      "object_number": 17,
-      "object": "onion",
-      "condition": "modified-slice",
-      "sentence_type": "modified",
-      "image_type": "slice",
-      "picture": "onion_slice.jpeg",
-      "sentence": "Someone slices an onion in front of you."
-    },
-    {
-      "object_number": 18,
-      "object": "mushrooms",
-      "condition": "modified-slice",
-      "sentence_type": "modified",
-      "image_type": "slice",
-      "picture": "mushrooms_slice.jpeg",
-      "sentence": "Someone slices mushrooms in front of you."
-    },
-    {
-      "object_number": 19,
-      "object": "radish",
-      "condition": "modified-whole",
-      "sentence_type": "modified",
-      "image_type": "whole",
-      "picture": "radish_whole.jpeg",
-      "sentence": "Someone slices a radish in front of you."
-    },
-    {
-      "object_number": 20,
-      "object": "pear",
-      "condition": "modified-whole",
-      "sentence_type": "modified",
-      "image_type": "whole",
-      "picture": "pear_whole.jpeg",
-      "sentence": "Someone slices a pear in front of you."
-    },
-    {
-      "object_number": 21,
-      "object": "pepper",
-      "condition": "modified-whole",
-      "sentence_type": "modified",
-      "image_type": "whole",
-      "picture": "pepper_whole.jpeg",
-      "sentence": "Someone slices a pepper in front of you."
-    },
-    {
-      "object_number": 22,
-      "object": "mango",
-      "condition": "modified-whole",
-      "sentence_type": "modified",
-      "image_type": "whole",
-      "picture": "mango_whole.jpeg",
-      "sentence": "Someone slices a mango in front of you."
-    },
-    {
-      "object_number": 23,
-      "object": "strawberry",
-      "condition": "modified-whole",
-      "sentence_type": "modified",
-      "image_type": "whole",
-      "picture": "strawberry_whole.jpeg",
-      "sentence": "Someone slices a strawberry in front of you."
-    },
-    {
-      "object_number": 24,
-      "object": "grapefruit",
-      "condition": "modified-whole",
-      "sentence_type": "modified",
-      "image_type": "whole",
-      "picture": "grapefruit_whole.jpeg",
-      "sentence": "Someone slices a grapefruit in front of you."
-    }
-  ]
+  {
+    "list": 2,
+    "object_number": 1,
+    "object": "watermelon",
+    "condition": "neutral-slice",
+    "sentence_type": "neutral",
+    "image_type": "slice",
+    "picture": "watermelon_slice.jpeg",
+    "image_path": "PICTURES/experimental-pics/watermelon_slice.jpeg",
+    "sentence": "Someone places a watermelon in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 2,
+    "object": "apple",
+    "condition": "neutral-slice",
+    "sentence_type": "neutral",
+    "image_type": "slice",
+    "picture": "apple_slice.jpeg",
+    "image_path": "PICTURES/experimental-pics/apple_slice.jpeg",
+    "sentence": "Someone places an apple in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 3,
+    "object": "cake",
+    "condition": "neutral-slice",
+    "sentence_type": "neutral",
+    "image_type": "slice",
+    "picture": "cake_slice.jpeg",
+    "image_path": "PICTURES/experimental-pics/cake_slice.jpeg",
+    "sentence": "Someone places a cake in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 4,
+    "object": "pie",
+    "condition": "neutral-slice",
+    "sentence_type": "neutral",
+    "image_type": "slice",
+    "picture": "pie _slice.jpeg",
+    "image_path": "PICTURES/experimental-pics/pie _slice.jpeg",
+    "sentence": "Someone places a pie in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 5,
+    "object": "bread",
+    "condition": "neutral-slice",
+    "sentence_type": "neutral",
+    "image_type": "slice",
+    "picture": "bread_slice.jpeg",
+    "image_path": "PICTURES/experimental-pics/bread_slice.jpeg",
+    "sentence": "Someone places bread in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 6,
+    "object": "pineapple",
+    "condition": "neutral-slice",
+    "sentence_type": "neutral",
+    "image_type": "slice",
+    "picture": "pineapple_slice.jpeg",
+    "image_path": "PICTURES/experimental-pics/pineapple_slice.jpeg",
+    "sentence": "Someone places a pineapple in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 7,
+    "object": "peach",
+    "condition": "neutral-whole",
+    "sentence_type": "neutral",
+    "image_type": "whole",
+    "picture": "peach_whole.jpeg",
+    "image_path": "PICTURES/experimental-pics/peach_whole.jpeg",
+    "sentence": "Someone places a peach in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 8,
+    "object": "steak",
+    "condition": "neutral-whole",
+    "sentence_type": "neutral",
+    "image_type": "whole",
+    "picture": "steak_whole.jpeg",
+    "image_path": "PICTURES/experimental-pics/steak_whole.jpeg",
+    "sentence": "Someone places a steak in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 9,
+    "object": "cheesecake",
+    "condition": "neutral-whole",
+    "sentence_type": "neutral",
+    "image_type": "whole",
+    "picture": "cheesecake_whole.jpeg",
+    "image_path": "PICTURES/experimental-pics/cheesecake_whole.jpeg",
+    "sentence": "Someone places a cheesecake in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 10,
+    "object": "lemon",
+    "condition": "neutral-whole",
+    "sentence_type": "neutral",
+    "image_type": "whole",
+    "picture": "lemon_whole.jpeg",
+    "image_path": "PICTURES/experimental-pics/lemon_whole.jpeg",
+    "sentence": "Someone places a lemon in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 11,
+    "object": "tomato",
+    "condition": "neutral-whole",
+    "sentence_type": "neutral",
+    "image_type": "whole",
+    "picture": "tomato_whole.jpeg",
+    "image_path": "PICTURES/experimental-pics/tomato_whole.jpeg",
+    "sentence": "Someone places a tomato in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 12,
+    "object": "orange",
+    "condition": "neutral-whole",
+    "sentence_type": "neutral",
+    "image_type": "whole",
+    "picture": "orange_whole.jpeg",
+    "image_path": "PICTURES/experimental-pics/orange_whole.jpeg",
+    "sentence": "Someone places an orange in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 13,
+    "object": "pizza",
+    "condition": "modified-slice",
+    "sentence_type": "modified",
+    "image_type": "slice",
+    "picture": "pizza_slice.jpeg",
+    "image_path": "PICTURES/experimental-pics/pizza_slice.jpeg",
+    "sentence": "Someone slices a pizza in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 14,
+    "object": "melon",
+    "condition": "modified-slice",
+    "sentence_type": "modified",
+    "image_type": "slice",
+    "picture": "melon_slice.jpeg",
+    "image_path": "PICTURES/experimental-pics/melon_slice.jpeg",
+    "sentence": "Someone slices a melon in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 15,
+    "object": "cucumber",
+    "condition": "modified-slice",
+    "sentence_type": "modified",
+    "image_type": "slice",
+    "picture": "cucumber_slice.jpeg",
+    "image_path": "PICTURES/experimental-pics/cucumber_slice.jpeg",
+    "sentence": "Someone slices a cucumber in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 16,
+    "object": "carrot",
+    "condition": "modified-slice",
+    "sentence_type": "modified",
+    "image_type": "slice",
+    "picture": "carrot_slice.jpeg",
+    "image_path": "PICTURES/experimental-pics/carrot_slice.jpeg",
+    "sentence": "Someone slices a carrot in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 17,
+    "object": "onion",
+    "condition": "modified-slice",
+    "sentence_type": "modified",
+    "image_type": "slice",
+    "picture": "onion_slice.jpeg",
+    "image_path": "PICTURES/experimental-pics/onion_slice.jpeg",
+    "sentence": "Someone slices an onion in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 18,
+    "object": "mushrooms",
+    "condition": "modified-slice",
+    "sentence_type": "modified",
+    "image_type": "slice",
+    "picture": "mushrooms_slice.jpeg",
+    "image_path": "PICTURES/experimental-pics/mushrooms_slice.jpeg",
+    "sentence": "Someone slices mushrooms in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 19,
+    "object": "radish",
+    "condition": "modified-whole",
+    "sentence_type": "modified",
+    "image_type": "whole",
+    "picture": "radish_whole.jpeg",
+    "image_path": "PICTURES/experimental-pics/radish_whole.jpeg",
+    "sentence": "Someone slices a radish in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 20,
+    "object": "pear",
+    "condition": "modified-whole",
+    "sentence_type": "modified",
+    "image_type": "whole",
+    "picture": "pear_whole.jpeg",
+    "image_path": "PICTURES/experimental-pics/pear_whole.jpeg",
+    "sentence": "Someone slices a pear in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 21,
+    "object": "pepper",
+    "condition": "modified-whole",
+    "sentence_type": "modified",
+    "image_type": "whole",
+    "picture": "pepper_whole.jpeg",
+    "image_path": "PICTURES/experimental-pics/pepper_whole.jpeg",
+    "sentence": "Someone slices a pepper in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 22,
+    "object": "mango",
+    "condition": "modified-whole",
+    "sentence_type": "modified",
+    "image_type": "whole",
+    "picture": "mango_whole.jpeg",
+    "image_path": "PICTURES/experimental-pics/mango_whole.jpeg",
+    "sentence": "Someone slices a mango in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 23,
+    "object": "strawberry",
+    "condition": "modified-whole",
+    "sentence_type": "modified",
+    "image_type": "whole",
+    "picture": "strawberry_whole.jpeg",
+    "image_path": "PICTURES/experimental-pics/strawberry_whole.jpeg",
+    "sentence": "Someone slices a strawberry in front of you."
+  },
+  {
+    "list": 2,
+    "object_number": 24,
+    "object": "grapefruit",
+    "condition": "modified-whole",
+    "sentence_type": "modified",
+    "image_type": "whole",
+    "picture": "grapefruit_whole.jpeg",
+    "image_path": "PICTURES/experimental-pics/grapefruit_whole.jpeg",
+    "sentence": "Someone slices a grapefruit in front of you."
+  }
+]
