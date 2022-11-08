a77ebee53a16a7872e810390d189ab6d135f837a
Nancy Xing <nxing@bowdoin.edu>
2022-11-08 17:24:23 -0500

    Added practice stimuli.

---
+++ practice.js
@@ -0,0 +1,42 @@
+var practicestimuli = [
+    {
+      "object_number": 1,
+      "object": "icecream",
+      "condition": "practice",
+      "image_path": "PICTURES/practice-pics/icecream.jpeg",
+      "sentence": "You look at ice cream.",
+      "correct_response": "A"
+    },
+    {
+      "object_number": 2,
+      "object": "stapler",
+      "condition": "practice",
+      "image_path": "PICTURES/practice-pics/key.jpeg",
+      "sentence": "You look at a stapler,",
+      "correct_response": "L"
+    },
+    {
+      "object_number": 3,
+      "object": "chocolate",
+      "condition": "practice",
+      "image_path": "PICTURES/practice-pics/chocolate.jpeg",
+      "sentence": "You look at chocolate.",
+      "correct_response": "A"
+    },
+    {
+      "object_number": 4,
+      "object": "leaf",
+      "condition": "practice",
+      "image_path": "PICTURES/practice-pics/leaf.jpeg",
+      "sentence": "You look at a leaf.",
+      "correct_response": "A"
+    },
+    {
+      "object_number": 5,
+      "object": "shrimp",
+      "condition": "practice",
+      "image_path": "PICTURES/practice-pics/blackberry.jpeg",
+      "sentence": "You look at shrimp.",
+      "correct_response": "L"
+    }
+  ]
